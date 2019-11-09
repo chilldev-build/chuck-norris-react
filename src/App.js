a4f649d578f4399a2e3f3270f9eb483562714cf4
Chris Hill <chrishillbuilds@gmail.com>
2019-11-09 15:33:57 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Quote from './components/quotes';
-import CategoryList from './components/categoryList'
+import CategoryList from './components/categoryList';
+
 import './App.css';
 
-class App extends Component {
-  state = {
-    category: "dev"
-  }
-
-  changeCategory = (category) => {
-    this.setState({
-        category
-    });
-  };
-
-  render() {
-    const { category } = this.state;
+function App() {
     return (
       <div className="App">
-          <Quote category ={category} />
-          <br />
-          <CategoryList
-            activeCategory={category}
-            changeCategory={category => this.changeCategory(category)}
-          />          
+        <header>
+          <h1>Chuck Reacts!</h1>
+        </header>
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+            </ul>
+          </nav>
+          <Route path='/' exact>
+            <CategoryList />
+          </Route>
+          <Route 
+            path='/category/:category_name?'
+            render={routeProps => <Quote {...routeProps} />}>
+          </Route>
+          {/* <Route>
+            path='/category/:category_name' component={Quote}
+          </Route> */}
+        </Router>
       </div>
     );
-  }
 }
 
 export default App;
